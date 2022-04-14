df9f7aac0e5eec9adf60dbc5f0ce72cde5eca098
Fariz <84852749+farizjs@users.noreply.github.com>
2022-04-14 21:10:14 +0700

    Update and rename __help.py to _help.py

--- userbot/modules/_help.py
+++ userbot/modules/_help.py
@@ -23,11 +23,11 @@ async def cmd_list(event):
         if args in CMD_HELP:
             await event.edit(f"**✘ Commands available in {args} ✘** \n\n" + str(CMD_HELP[args]) + "\n\n**💕 @TheFlicksUserbot**")
         else:
-            await event.edit(f"**Module** `{args}` **Tidak tersedia!**")
+            await event.edit(f"**Module** `{args}` **Not available!**")
     else:
         try:
             results = await bot.inline_query(  # pylint:disable=E0602
-                BOT_USERNAME, "@FlicksSupport"
+                BOT_USERNAME, "galaxyo"
             )
             await results[0].click(
                 event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
