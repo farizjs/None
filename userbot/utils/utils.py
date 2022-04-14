1212e9171af726c7b582dcbbb43d96de7b95f90a
Fariz <84852749+farizjs@users.noreply.github.com>
2022-04-14 17:17:45 +0700

    Create utils.py

---
+++ userbot/utils/utils.py
@@ -0,0 +1,166 @@
+# Credits: @mrismanaziz
+# FROM Man-Userbot <https://github.com/mrismanaziz/Man-Userbot>
+# t.me/SharingUserbot & t.me/Lunatic0de
+
+import asyncio
+import importlib
+import logging
+import sys
+from pathlib import Path
+from random import randint
+
+import heroku3
+from telethon.tl.functions.contacts import UnblockRequest
+
+from userbot import (
+    BOT_TOKEN,
+    BOTLOG_CHATID,
+    CMD_HELP,
+    HEROKU_API_KEY,
+    HEROKU_APP_NAME,
+    LOGS,
+    bot,
+)
+
+heroku_api = "https://api.heroku.com"
+if HEROKU_APP_NAME is not None and HEROKU_API_KEY is not None:
+    Heroku = heroku3.from_key(HEROKU_API_KEY)
+    app = Heroku.app(HEROKU_APP_NAME)
+    heroku_var = app.config()
+else:
+    app = None
+
+
+async def autobot():
+    if BOT_TOKEN:
+        return
+    await bot.start()
+    await bot.send_message(
+        BOTLOG_CHATID, "**IS BUILDING A TELEGRAM BOTT FOR YOU AT @BotFather**"
+    )
+    who = await bot.get_me()
+    name = who.first_name + " Assistant Bot"
+    if who.username:
+        username = who.username + "_ubot"
+    else:
+        username = "glx" + (str(who.id))[5:] + "ubot"
+    bf = "@BotFather"
+    await bot(UnblockRequest(bf))
+    await bot.send_message(bf, "/cancel")
+    await asyncio.sleep(1)
+    await bot.send_message(bf, "/start")
+    await asyncio.sleep(1)
+    await bot.send_message(bf, "/newbot")
+    await asyncio.sleep(1)
+    isdone = (await bot.get_messages(bf, limit=1))[0].text
+    if isdone.startswith("That I cannot do."):
+        LOGS.info(
+            "Please create a Bot from @BotFather and add its token in var BOT_TOKEN"
+        )
+        sys.exit(1)
+    await bot.send_message(bf, name)
+    await asyncio.sleep(1)
+    isdone = (await bot.get_messages(bf, limit=1))[0].text
+    if not isdone.startswith("Good."):
+        await bot.send_message(bf, "My Assistant Bot")
+        await asyncio.sleep(1)
+        isdone = (await bot.get_messages(bf, limit=1))[0].text
+        if not isdone.startswith("Good."):
+            LOGS.info(
+                "Please create a Bot from @BotFather and add its token in var BOT_TOKEN"
+            )
+            sys.exit(1)
+    await bot.send_message(bf, username)
+    await asyncio.sleep(1)
+    isdone = (await bot.get_messages(bf, limit=1))[0].text
+    await bot.send_read_acknowledge("botfather")
+    if isdone.startswith("Sorry,"):
+        ran = randint(1, 100)
+        username = "xflicks" + (str(who.id))[6:] + str(ran) + "ubot"
+        await bot.send_message(bf, username)
+        await asyncio.sleep(1)
+        nowdone = (await bot.get_messages(bf, limit=1))[0].text
+        if nowdone.startswith("Done!"):
+            token = nowdone.split("`")[1]
+            await bot.send_message(bf, "/setinline")
+            await asyncio.sleep(1)
+            await bot.send_message(bf, f"@{username}")
+            await asyncio.sleep(1)
+            await bot.send_message(bf, "Search")
+            await asyncio.sleep(3)
+            await bot.send_message(bf, "/setuserpic")
+            await asyncio.sleep(1)
+            await bot.send_message(bf, f"@{username}")
+            await asyncio.sleep(1)
+            await bot.send_file(bf, "userbot/files/20220218_190645.jpg")
+            await asyncio.sleep(3)
+            await bot.send_message(bf, "/setabouttext")
+            await asyncio.sleep(1)
+            await bot.send_message(bf, f"@{username}")
+            await asyncio.sleep(1)
+            await bot.send_message(bf, f"Managed By @{who.username}")
+            await asyncio.sleep(3)
+            await bot.send_message(bf, "/setdescription")
+            await asyncio.sleep(1)
+            await bot.send_message(bf, f"@{username}")
+            await asyncio.sleep(1)
+            await bot.send_message(
+                bf, f"✨ Owner ~ @{who.username} ✨\n\n✨ Powered By ~ @TheFlicksUserbot ✨"
+            )
+            await bot.send_message(
+                BOTLOG_CHATID,
+                f"**SUCCESSFULLY CREATING A TELEGRAM BOTT WITH USERNAME @{username}**",
+            )
+            await bot.send_message(
+                BOTLOG_CHATID,
+                "**Wait a moment, Restarting Heroku to Apply Changes.**",
+            )
+            heroku_var["BOT_TOKEN"] = token
+            heroku_var["BOT_USERNAME"] = f"{username}"
+        else:
+            LOGS.info(
+                "Please Delete Some of your Telegram Bots at @Botfather or Set Var BOT_TOKEN with bot tokens"
+            )
+            sys.exit(1)
+    elif isdone.startswith("Done!"):
+        token = isdone.split("`")[1]
+        await bot.send_message(bf, "/setinline")
+        await asyncio.sleep(1)
+        await bot.send_message(bf, f"@{username}")
+        await asyncio.sleep(1)
+        await bot.send_message(bf, "Search")
+        await asyncio.sleep(3)
+        await bot.send_message(bf, "/setuserpic")
+        await asyncio.sleep(1)
+        await bot.send_message(bf, f"@{username}")
+        await asyncio.sleep(1)
+        await bot.send_file(bf, "userbot/files/20220218_190645.jpg")
+        await asyncio.sleep(3)
+        await bot.send_message(bf, "/setabouttext")
+        await asyncio.sleep(1)
+        await bot.send_message(bf, f"@{username}")
+        await asyncio.sleep(1)
+        await bot.send_message(bf, f"Managed By @{who.username}")
+        await asyncio.sleep(3)
+        await bot.send_message(bf, "/setdescription")
+        await asyncio.sleep(1)
+        await bot.send_message(bf, f"@{username}")
+        await asyncio.sleep(1)
+        await bot.send_message(
+            bf, f"✨ Owner ~ @{who.username} ✨\n\n✨ Powered By ~ @TheFlicksUserbot ✨"
+        )
+        await bot.send_message(
+            BOTLOG_CHATID,
+            f"**SUCCESSFULLY CREATING A TELEGRAM BOTT WITH USERNAME @{username}**",
+        )
+        await bot.send_message(
+            BOTLOG_CHATID,
+            "**Wait a moment, Restarting Heroku to Apply Changes.**",
+        )
+        heroku_var["BOT_TOKEN"] = token
+        heroku_var["BOT_USERNAME"] = f"{username}"
+    else:
+        LOGS.info(
+            "Please Delete Some of your Telegram Bots at @Botfather or Set Var BOT_TOKEN with bot tokens"
+        )
+        sys.exit(1)
